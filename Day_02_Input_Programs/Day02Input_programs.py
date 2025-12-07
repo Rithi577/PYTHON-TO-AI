bd202735ef8e426a8fe16d257cc9b4559b9fe827
Rithika <rithirithi341@gmail.com>
2025-12-08 00:37:40 +0530

    Add input programs for basic calculations and conversions

---
+++ Day_02_Input_Programs/Day02Input_programs.py
@@ -0,0 +1,40 @@
+# Day 02 - Input Programs
+
+
+# 1. Add two numbers using input
+a = int(input("Enter number: "))
+b = int(input("Enter another number: "))
+print("The sum is", a + b)
+
+
+
+# 2. Age after 10 years
+age = int(input("Enter your age: "))
+future_age = age + 10
+print("You will be", future_age, "years old after 10 years")
+
+
+
+# 3. Area of rectangle using input
+length = int(input("Enter length: "))
+breadth = int(input("Enter breadth: "))
+area = length * breadth
+print("Area is", area)
+
+
+
+# 4. Minutes to hours conversion
+minutes = int(input("Enter minutes: "))
+hours = minutes / 60
+print("Hours:", hours)
+
+
+
+# 5. Simple calculator
+x = int(input("Enter number 1: "))
+y = int(input("Enter number 2: "))
+
+print("Sum:", x + y)
+print("Difference:", x - y)
+print("Product:", x * y)
+print("Division:", x / y)
